2cf34412f75a4add7df88378d1ba600d2c51a35a
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-11-04 08:36:19 -0500

    Added Portfolio Component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+export class Portfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check out my work</h1>
+            <div className="bgrid-quarters s-bgrid-thirds cf" alt="">
+              {resumeData.portfolio &&
+                resumeData.portfolio.map((item) => {
+                  return (
+                    <div className="columns portfolio-item">
+                      <div className="item-wrap">
+                        <a href="#modal-01">
+                          <img
+                            src={`${item.imgurl}`}
+                            className="item-img"
+                            alt=""
+                          />
+                          <div className="overlay">
+                            <div className="portfolio-item-meta">
+                              <h5>{item.name}</h5>
+                              <p>{item.description}</p>
+                            </div>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
